74e9f342da01c1749cb96e09126c515de9205803
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-03-07 15:48:03 +0900

    [level 0] Title: 소인수분해, Time: 3.17 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        
+        for(int i = 2; i <= n; i++) {
+            while(n % i == 0) {
+                set.add(i);
+                n /= i;
+            }
+        }
+        if(n != 1) set.add(n);
+        
+        return set.stream().mapToInt(Integer::intValue).sorted().toArray();
+    }
+}
