4fb87df4a0e9ee45f6619e0237de7767faa8cf75
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-03-08 15:36:35 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.24 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,12 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int solution(String s) {
         int answer = 0;
         String[] str = s.split(" ");
         
+        // for(int i = 0; i < str.length; i++) {
+        //     if(str[i].equals("Z")) answer -= Integer.parseInt(str[i - 1]);
+        //     else answer += Integer.parseInt(str[i]);
+        // }
+        
+        Stack<Integer> st = new Stack<Integer>();
         for(int i = 0; i < str.length; i++) {
-            if(str[i].equals("Z")) answer -= Integer.parseInt(str[i - 1]);
-            else answer += Integer.parseInt(str[i]);
+            if(!str[i].equals("Z")) st.push(Integer.parseInt(str[i]));
+            else st.pop();
         }
+        
+        while(st.size() != 0)
+            answer += st.pop();
         return answer;
     }
 }
