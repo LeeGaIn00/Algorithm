1d360b50a4e87a0adb256da6eac166a9b8175404
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-03-11 13:33:13 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -2,10 +2,9 @@ class Solution {
     public String solution(String[] id_pw, String[][] db) {
         for(int i = 0; i < db.length; i++) {
             if(db[i][0].equals(id_pw[0])) {
-                for(int j = 0; j < db[0].length; j++) {
-                    if(db[i][j].equals(id_pw[1])) return "login";
-                }
-                return "wrong pw";
+                if(db[i][1].equals(id_pw[1])) 
+                    return "login";
+                else return "wrong pw";
             }
         }
         return "fail";
