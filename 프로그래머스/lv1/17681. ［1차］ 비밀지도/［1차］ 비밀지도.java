3647756499a1999934211b3bb71f0416287c1f84
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-03-28 20:53:35 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.10 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,22 +1,39 @@
+// class Solution {
+//     public String[] solution(int n, int[] arr1, int[] arr2) {
+//         String[] answer = new String[n];
+//         Long[][] map1 = new Long[n][n];
+//         Long[][] map2 = new Long[n][n];
+        
+//         for(int i = 0; i < arr1.length; i++) {
+//             char[] bin1 = String.format("%0" + n + "d", Long.parseLong(Long.toBinaryString(arr1[i]))).toCharArray();
+//             char[] bin2 = String.format("%0" + n + "d", Long.parseLong(Long.toBinaryString(arr2[i]))).toCharArray();
+//             String temp = "";
+            
+//             for(int j = 0; j < n; j++) {
+//                 if(bin1[j] == '1' || bin2[j] == '1')
+//                     temp += "#";
+//                 else 
+//                     temp += " ";
+//             }
+//             answer[i] = temp;
+//         }
+//         return answer;
+//     }
+// }
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        Long[][] map1 = new Long[n][n];
-        Long[][] map2 = new Long[n][n];
         
-        for(int i = 0; i < arr1.length; i++) {
-            char[] bin1 = String.format("%0" + n + "d", Long.parseLong(Long.toBinaryString(arr1[i]))).toCharArray();
-            char[] bin2 = String.format("%0" + n + "d", Long.parseLong(Long.toBinaryString(arr2[i]))).toCharArray();
-            String temp = "";
+        for(int i = 0; i < n; i++) {
+            String bin = Long.toBinaryString(arr1[i] | arr2[i]);
+            answer[i] = bin.replace("1", "#").replace("0", " ");
+            //answer[i] = bin.length() < n ? " " + bin : bin;
             
-            for(int j = 0; j < n; j++) {
-                if(bin1[j] == '1' || bin2[j] == '1')
-                    temp += "#";
-                else 
-                    temp += " ";
+            while (answer[i].length() < n) {
+                answer[i] = ' ' + answer[i];
             }
-            answer[i] = temp;
         }
+        
         return answer;
     }
 }
