349d370c3c039befd63669eeb12e90d2d95118bd
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-04-03 18:48:44 +0900

    [level 1] Title: 폰켓몬, Time: 9.13 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] nums) {
+        int N = nums.length;
+        nums = Arrays.stream(nums).distinct().toArray();
+        
+        if(N / 2 > nums.length) return nums.length;
+        else return N/2;
+    }
+}
