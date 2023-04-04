4fb61c2a6e4a1eccce2d6c1f7d5e0f15c75eaaec
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-04-04 22:54:57 +0900

    [Bronze IV] Title: 기찍 N, Time: 1340 ms, Memory: 32328 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t=in.nextInt();
+		int i;
+		for(i=t;i>=1;i--) 
+			System.out.println(i); 
+	}
+}
