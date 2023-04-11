d2e22a48bb0df444367ef29650ffab9a7b549b9c
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-04-12 00:11:15 +0900

    [Silver IV] Title: 주몽, Time: 200 ms, Memory: 16448 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(a);
+        
+        int start = 0;
+        int end = n - 1;
+        int count = 0;
+        while(start < end) {
+            if(a[start] + a[end] < m) {
+                start++;
+            }
+            else if(a[start] + a[end] > m) {
+                end--;
+            }
+            else {
+                count++;
+                start++;
+                end--;
+            }
+        }
+		System.out.println(count);
+	}
+}
