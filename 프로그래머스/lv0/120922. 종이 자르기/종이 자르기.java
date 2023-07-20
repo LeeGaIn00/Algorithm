3271a88b0d58189070db81b7f2deae3bf9420dd1
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-07-20 16:35:47 +0900

    [level 0] Title: 종이 자르기, Time: 0.01 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.java
+++ 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.java
@@ -1,8 +1,5 @@
 class Solution {
     public int solution(int M, int N) {
-        if(M == 1 && N == 1) return 0;
-        else if(M == 1) return N - 1;
-        else if(N == 1) return M - 1;
-        else return M * N - 1;
+        return M * N - 1;
     }
 }
