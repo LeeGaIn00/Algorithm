f7659dc67f1a315b394e0cc88daaf69107b4b65f
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-07-20 16:21:01 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int[] common) {
-        // 등차 
-        if(2 * common[1] == common[0] + common[2])
-            return common[common.length - 1] + (common[1] - common[0]);
-        // 등비
-        else 
-            return common[common.length - 1] * (common[1] / common[0]);
+        int answer = 0;
+        if(common[1]-common[0] == common[2] - common[1]) 
+            answer = common[common.length-1] + (common[1]-common[0]);
+        else
+            answer = common[common.length-1] * (common[1]/common[0]);
+        return answer;
     }
 }
