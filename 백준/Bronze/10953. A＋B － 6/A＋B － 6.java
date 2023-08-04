e0cf96873db8b33a8e53c0378d2e177010802c26
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-04 22:39:56 +0900

    [Bronze III] Title: A+B - 6, Time: 224 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -1,15 +1,13 @@
 import java.util.*;
 
-public class Main {
+public class Main
+{
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for(int i = 0; i < T; i++) {
-            String str = sc.next();
-            StringTokenizer st = new StringTokenizer(str, ",");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            System.out.println(a+b);
-        }
+	    int n = sc.nextInt();
+	    for(int i = 0; i < n; i++) {
+	        String s[] = sc.next().split(",");
+	        System.out.println(Integer.parseInt(s[0]) + Integer.parseInt(s[1]));
+	    }
 	}
 }
