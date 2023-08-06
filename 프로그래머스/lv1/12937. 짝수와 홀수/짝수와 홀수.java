c6a7b1ca7e7582d7aa70132e4eb4f753529442d6
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-06 19:14:12 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,10 +1,6 @@
 class Solution {
     public String solution(int num) {
-      
-        if(num % 2 == 0)
-            return "Even";
-        else
-            return "Odd";
-     
+        String answer = num % 2 == 0 ? "Even" : "Odd";
+        return answer;
     }
 }
