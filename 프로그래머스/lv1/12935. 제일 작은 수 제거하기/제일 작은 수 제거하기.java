eeba0938be06ee640577b31f8278bfb2de58d7db
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-07 16:33:01 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.51 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -3,21 +3,17 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
         int[] answer = {};
-        if(arr.length == 1) {
-            answer = new int[1];
-            answer[0] = -1;
-        }
-        else {
-            int min = arr[0];
-            for(int i = 1; i < arr.length; i++)
-                min = Math.min(min, arr[i]);
-            ArrayList<Integer> list = new ArrayList<>();
-            for(int i = 0; i < arr.length; i++)
-                if(arr[i] != min) list.add(arr[i]);
-            answer = new int[list.size()];
-            for(int i = 0; i < answer.length; i++)
-                answer[i] = list.get(i);
-        }
+        if(arr.length == 1) return new int[]{-1};
+        int min = arr[0];
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 1; i < arr.length; i++)
+            min = Math.min(min, arr[i]);
+        for(int i = 0; i < arr.length; i++)
+            if(arr[i] != min) list.add(arr[i]);
+        answer = new int[list.size()];
+        for(int i = 0; i < answer.length; i++)
+            answer[i] = list.get(i);
+        
         return answer;
     }
 }
