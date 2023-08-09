bde7a704951c844ebfabe1ffda4cfc3f633398e5
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-09 13:55:42 +0900

    [level 1] Title: 예산, Time: 0.53 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.java
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -3,25 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        int total = 0;
-        
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        
-        for(int i = 0; i < d.length; i++)
-            list.add(d[i]);
-        
-        Collections.sort(list);
-        
-        for(int i = 0; i < list.size(); i++) {
-            total += list.get(i);
-            if(total > budget) {
-                answer = i;
-                break;
-            }   
+        Arrays.sort(d);
+        for(int i = 0; i < d.length; i++) {
+            if(budget - d[i] >= 0) {
+                budget -= d[i];
+                answer++;
+            }
         }
-        
-        if(total <= budget) answer = list.size();
-        
         return answer;
     }
 }
