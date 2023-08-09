d5ae5dbf1dd98989eee6adafa84c2fffbc7e80d5
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-09 16:29:51 +0900

    [level 1] Title: K번째수, Time: 0.32 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -3,26 +3,13 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        
-        
-//         for(int i = 0; i < commands[0].length; i++) {
-//             int[] temp = new int[commands[i][1] - commands[i][0] + 1];
-            
-//             for(int j = 0; j <= commands[i][1] - commands[i][0]; j++)
-//                 temp[j] = array[commands[i][0] + j - 1];
-//             Arrays.sort(temp); 
-//             answer[i] = temp[commands[i][2] - 1];
-//         }
-
-		for (int i = 0; i < commands.length; i++) {
-			int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]); 
-            					   // 원본 배열, 복사할 시작인덱스, 복사할 끝인덱스
-
-			Arrays.sort(temp); // 배열 오름차순 정렬
-			answer[i] = temp[commands[i][2] - 1];
-		}
-        
-        
+        for(int i = 0; i < commands.length; i++) {
+            ArrayList<Integer> list = new ArrayList<>();
+            for(int j = commands[i][0] - 1; j < commands[i][1]; j++) 
+                list.add(array[j]);
+            Collections.sort(list);
+            answer[i] = list.get(commands[i][2] - 1);
+        }
         return answer;
     }
 }
