0f7016035614b34d3e344efb8630cd59e8831881
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-12 21:56:34 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 10.16 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,20 +1,9 @@
-import java.util.*;
-
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-        List<String> list = new ArrayList<>();
-        list.add("0"); // 가운데 물
-    
-        for(int i = food.length - 1; i > 0; i--) {
-            for(int j = 0; j < food[i] / 2; j++) {
-                list.add(0, Integer.toString(i));
-                list.add(Integer.toString(i));
-            }        
-        }
-        
-        for(String i : list)
-            answer += i;
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i < food.length; i++)
+            for(int j = 0; j < food[i]/2; j++)
+                sb.append(i);
+        return sb.toString() + "0" + sb.reverse().toString();
     }
 }
