6fe7f814aeb4884e2a6969fcb12e193ec603a6a7
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-13 20:22:24 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.21 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -3,16 +3,12 @@ import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        List<Integer> list = new ArrayList<>();
-        
+        ArrayList<Integer> list = new ArrayList<>();
         for(int i = 0; i < score.length; i++) {
             list.add(score[i]);
-            Collections.sort(list, Collections.reverseOrder());
-            
-            if(i < k)
-                answer[i] = list.get(list.size()-1);
-            else 
-                answer[i] = list.get(k-1);
+            Collections.sort(list);
+            if(i < k) answer[i] = list.get(0);
+            else answer[i] = list.get(list.size() - k);
         }
         return answer;
     }
