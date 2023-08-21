1577faf288fb1d504cf9e09b407a366ee7ac7d8f
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-21 21:20:36 +0900

    [level 1] Title: 소수 찾기, Time: 14.23 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -1,21 +1,19 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int[] arr = new int[n + 1];
-        
-        for(int i = 2; i <= n; i++) 
-            arr[i] = i;
-        
-        for(int i = 2; i <= n; i++) {
-            if(arr[i] != 0)
-                for(int j = 2 * i; j <= n; j += i){
-                    arr[j] = 0;
-                }
-        }
-        
+        boolean prime[] = new boolean[n + 1];
+        prime[0] = prime[1] = true;
         for(int i = 2; i <= n; i++)
-            if(arr[i] != 0) answer++;
+            prime[i] = true;
         
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(prime[i]) {
+                for(int j = i; i*j <= n; j++)
+                    prime[i*j] = false;
+            }
+        }
+        for(int i = 2; i <= n; i++)
+            if(prime[i]) answer++;
         return answer;
     }
 }
