d8c0a4bf9e54acb33051d16f7a5985000fa603ea
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-23 23:37:35 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,26 +1,15 @@
 import java.util.*;
- 
-public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String str = sc.next();
-        int[] A = new int[str.length()];
-        for(int i = 0; i < str.length(); i++) 
-            A[i] = Integer.parseInt(str.substring(i, i+1));
-        
-        for(int i = 0; i < str.length(); i++ ) {
-            int max = i;
-            for(int j = i + 1; j < str.length(); j++) {
-                if(A[j] > A[max]) 
-                    max = j;
-            }
-            if(A[i] < A[max]) {
-                int temp = A[i];
-                A[i] = A[max];
-                A[max] = temp;
-            }
-        }
-        for(int i = 0; i < str.length(); i++)
-            System.out.print(A[i]);
-	}
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String arr[] = br.readLine().split("");
+        StringBuilder sb = new StringBuilder();
+        Arrays.sort(arr);
+        for(int i = arr.length - 1; i >= 0; i--)
+            sb.append(arr[i]);
+        System.out.println(sb);
+    }
 }
