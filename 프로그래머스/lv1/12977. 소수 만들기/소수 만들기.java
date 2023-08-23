cbc74ee0e60f3d736042882c7fb8c84b7e7e3b68
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-23 22:58:35 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -1,21 +1,15 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        int sum = 0;
-        
         for(int i = 0; i < nums.length - 2; i++)
-            for(int j = i + 1; j < nums.length - 1; j++)
+            for(int j = i + 1; j < nums.length -1; j++)
                 for(int k = j + 1; k < nums.length; k++) {
-                    sum = nums[i] + nums[j] + nums[k];
-                    if(isPrime(sum)) answer++;
+                    if(isPrime(nums[i] + nums[j] + nums[k])) answer++;
                 }
-                   
         return answer;
     }
     boolean isPrime(int n) {
-        for(int i = 2; i < n; i++)
+        for(int i = 2; i < n/2; i++)
             if(n % i == 0) return false;
         return true;
     }
