3257d8f28dc811bacd8f0811d47d5021482da6f1
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-25 00:11:05 +0900

    [level 0] Title: 다항식 더하기, Time: 8.20 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
@@ -1,21 +1,29 @@
 class Solution {
     public String solution(String polynomial) {
         String answer = "";
-        String[] temp = polynomial.replace(" ", "").split("\\+");
-        int[] k = {0, 0};
+        int x = 0;
+        int c = 0;
+        String str[] = polynomial.replace(" ", "").split("\\+");
         
-        for(int i = 0; i < temp.length; i++) {
-            if(temp[i].contains("x")){
-                if(temp[i].equals("x")) k[0] += 1;
-                else k[0] += Integer.parseInt(temp[i].replace("x", ""));
-            }
+        for(int i = 0; i < str.length; i++) {
+            if(str[i].equals("x")) x += 1;
+            else if(str[i].contains("x")) 
+                x += Integer.parseInt(str[i].substring(0, str[i].length() - 1));
             else 
-               k[1] += Integer.parseInt(temp[i]);  
+                c += Integer.parseInt(str[i]);
         }
-        if(k[0] != 0 && k[1] == 0) answer = (k[0] == 1 ? "" : k[0]) + "x";
-        else if((k[0] == 0 && k[1] != 0) || (k[0] == 0 && k[1] == 0)) answer = k[1] + "";
-        else answer = (k[0] == 1 ? "" : k[0]) + "x + " + k[1];
-        
+        if(x != 0 && c != 0) {
+            if(x == 1) answer = "x + " + c;
+            else answer = x + "x + " + c;
+        }
+        else if(x == 0 && c != 0) 
+               answer = c + "";
+        else if(x != 0 && c == 0) {
+             if(x == 1) answer = "x";
+             else answer = x + "x";
+        }
+        else 
+               answer = "0";
         return answer;
     }
 }
