b67fcd6aac23357f6adbdb676220e3a5eda0e8bd
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-25 19:43:24 +0900

    [level 1] Title: 모의고사, Time: 1.00 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -2,25 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] count = new int[3];
-        int[] arr1 = {1, 2, 3 ,4 ,5};
-        int[] arr2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        ArrayList<Integer> rank = new ArrayList<Integer>();
+        int[] answer = {};
+        int p1[] = {1, 2, 3, 4, 5};
+        int p2[] = {2, 1, 2, 3, 2, 4, 2, 5};
+        int p3[] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int score[] = {0, 0, 0};
         
-        for(int i = 0; i < answers.length; i++) {
-            if(answers[i] == arr1[i % arr1.length]) count[0]++;
-            if(answers[i] == arr2[i % arr2.length]) count[1]++;
-            if(answers[i] == arr3[i % arr3.length]) count[2]++;
+        for(int i = 0; i < answers.length; i ++) {
+            if(answers[i] == p1[i % 5]) score[0]++;
+            if(answers[i] == p2[i % 8]) score[1]++;
+            if(answers[i] == p3[i % 10]) score[2]++;
         }
+        ArrayList<Integer> list = new ArrayList<>();
+        int max = Math.max(score[0], Math.max(score[1], score[2]));
         
-        int max = Math.max(count[0], Math.max(count[1], count[2]));
         for(int i = 0; i < 3; i++)
-            if(count[i] == max) rank.add(i + 1);
+            if(score[i] == max) list.add(i + 1);
         
-        int[] answer = new int[rank.size()];
-        for(int i = 0; i < rank.size(); i++)
-            if(rank.get(i) != 0) answer[i] = rank.get(i);
+        answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++)
+            answer[i] = list.get(i);
         return answer;
     }
 }
