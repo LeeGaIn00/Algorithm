3c9c21979d69fdb82a87dd20c3978babbb96823d
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-27 00:24:10 +0900

    [Bronze I] Title: 단어순서 뒤집기, Time: 140 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
@@ -1,24 +1,19 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main
+{
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        Stack<String> st = new Stack<>();
-        
         for(int i = 0; i < n; i++) {
-            String answer = "";
-            String str = br.readLine();
-            String[] arr = str.split(" ");
-            
-            for(int j = 0; j < arr.length; j++)
-                st.push(arr[j]);
-            for(int j = 0; j < arr.length; j++)
-                answer += " " + st.pop();
-            System.out.println("Case #" + (i+1) + ":" + answer);
+            StringBuilder sb = new StringBuilder();
+            String s = br.readLine();
+            String str[] = s.split(" ");
+            sb.append("Case #" + (i + 1) + ": ");
+            for(int j = str.length - 1; j >= 0; j--)
+                sb.append(str[j] + " ");
+            System.out.println(sb);
         }
-	}
-
+    }
 }
