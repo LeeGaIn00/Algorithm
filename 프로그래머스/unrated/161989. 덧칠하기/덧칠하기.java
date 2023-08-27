d6f4940a2eda2e9cce072471954241a16a754253
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-27 15:56:43 +0900

    [unrated] Title: 덧칠하기, Time: 1.00 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -1,15 +1,15 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        int max = 0;
-    
-        for(int i = 0; i < section.length; i++) {
-            if(section[i] < max) 
-                continue;
-            else {
+        
+        int start = section[0];
+        answer++;
+        
+        for(int s : section) {
+            if(start + m <= s) {
+                start = s;
                 answer++;
-                max = section[i] + m;
-            }    
+            }
         }
         return answer;
     }
