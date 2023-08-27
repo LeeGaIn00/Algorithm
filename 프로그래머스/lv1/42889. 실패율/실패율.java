12333a3bf6f2641707c59f20785d9b19ffc1ec62
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-27 16:44:47 +0900

    [level 1] Title: 실패율, Time: 1.26 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.java
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -3,29 +3,34 @@ import java.util.*;
 class Solution {
     public int[] solution(int N, int[] stages) {
         int[] answer = new int[N];
-        double[] fail = new double[N];
-        Map<Integer, Double> m = new HashMap<>();
         
-        for(int i = 0; i < N; i++) {
-            int count = 0; // 도전자 수
-            int num = 0; // 실패한 사람 수
+        Map<Integer, Double> rank = new HashMap<>();
+        Arrays.sort(stages);
+        
+        for(int i = 1; i <= N; i++) {
+            int current = 0;
+            int success = 0;
+            
             for(int j = 0; j < stages.length; j++) {
-                if(stages[j] >= i+1) count++;
-                if(stages[j] == i+1) num++;
+                if(stages[j] == i) {
+                    current++;
+                    success++;
+                }
+                else if(stages[j] > i) {
+                    success += stages.length - j;
+                    break;
+                }
             }
-            if(count == 0) fail[i] = 0;
-            else fail[i] = num / (double)count;
+            
+            if(success == 0) rank.put(i, 0.0);
+            else rank.put(i, (double)current / success);
         }
+        List<Integer> keySet = new ArrayList<>(rank.keySet());
+        Collections.sort(keySet, (o1, o2) -> (rank.get(o2).compareTo(rank.get(o1))));
         
-        for(int i = 0; i < fail.length; i++) 
-            m.put(i+1, fail[i]);
-    
-        List<Integer> keyList = new ArrayList<>(m.keySet());
-        keyList.sort((o1, o2) -> m.get(o2).compareTo(m.get(o1)));
-        
-        int i = 0;
-        for (int key : keyList) 
-            answer[i++] = key;
+        int idx = 0;
+        for(int i : keySet)
+            answer[idx++] = i;
         
         return answer;
     }
