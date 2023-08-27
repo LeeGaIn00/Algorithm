9684c0d5a0954c275dd6e2353ecdf0bcfb2a922f
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-27 17:47:43 +0900

    [unrated] Title: 기사단원의 무기, Time: 2.33 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,25 +1,24 @@
 class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
-        int[] arr = new int[number];
-        arr[0] = 1;
+        int arr[] = new int[number + 1];
         
-        for(int i = 1; i < number; i++)
-            arr[i] = divisor(i+1);
+        for(int i = 1; i <= number; i++) 
+            arr[i] = count(i);
         
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] > limit) answer += power;
-            else answer += arr[i];
-        }
+        for(int i = 1; i <= number; i++) 
+            answer += arr[i] > limit ? power : arr[i]; 
+        
         return answer;
     }
-    // 약수의 개수
-    int divisor(int n) {
-        int cnt = 0;
-        for(int i = 1; i * i <= n; i++) {
-            if(i * i == n) cnt++;
-            else if(n % i == 0) cnt += 2;
-        }
+    
+    public int count(int n) {
+        if(n == 1) return 1;
+        
+        int cnt = 2;
+        for(int i = 2; i <= n / 2; i++)
+            if(n % i == 0) cnt++;
+        
         return cnt;
     }
 }
