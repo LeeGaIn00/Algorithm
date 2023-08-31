d8c51357cfff46bdeb993df11c33575679fc06db
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-31 15:17:26 +0900

    [level 2] Title: 주식가격, Time: 25.80 ms, Memory: 64.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> st = new Stack<>();
+        
+        for(int i = 0; i < prices.length; i++) {
+            // 가격 떨어졌다면
+            while(!st.isEmpty() && prices[st.peek()] > prices[i]) {
+                answer[st.peek()] = i - st.peek();
+                st.pop();
+            }
+            st.push(i);    
+        }
+        
+        while(!st.isEmpty()) {
+            answer[st.peek()] = prices.length - st.peek() - 1;
+            st.pop();
+        }
+        
+        return answer;
+    }
+}
