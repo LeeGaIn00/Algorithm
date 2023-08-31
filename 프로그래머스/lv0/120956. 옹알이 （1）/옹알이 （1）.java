e32e728e4e72db56f06a7c170e9882bbdd00fd5e
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-31 15:47:30 +0900

    [level 0] Title: 옹알이 (1), Time: 0.13 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,14 +1,12 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
+        String arr[] = {"aya", "ye", "woo", "ma"};
         
         for(int i = 0; i < babbling.length; i++) {
-            if(babbling[i].contains("ayaaya") || babbling[i].contains("yeye") || babbling[i].contains("woowoo") || babbling[i].contains("mama")) continue;
+            for(int j = 0; j < 4; j++) 
+                babbling[i] = babbling[i].replace(arr[j], " ");
             
-            babbling[i] = babbling[i].replace("aya", " ");
-            babbling[i] = babbling[i].replace("ye", " ");
-            babbling[i] = babbling[i].replace("woo", " ");
-            babbling[i] = babbling[i].replace("ma", " ");
             babbling[i] = babbling[i].replace(" ", "");
             
             if(babbling[i].length() == 0) answer++;
