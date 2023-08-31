cb3063d43f9b8192a20e2bd49e0e9eaacea39f82
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-08-31 15:19:45 +0900

    [level 2] Title: 주식가격, Time: 11.96 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -3,21 +3,28 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Stack<Integer> st = new Stack<>();
         
         for(int i = 0; i < prices.length; i++) {
-            // 가격 떨어졌다면
-            while(!st.isEmpty() && prices[st.peek()] > prices[i]) {
-                answer[st.peek()] = i - st.peek();
-                st.pop();
+            for(int j = i + 1;  j < prices.length; j++) {
+                answer[i]++;
+                if(prices[i] > prices[j]) break; 
             }
-            st.push(i);    
         }
+//         Stack<Integer> st = new Stack<>();
         
-        while(!st.isEmpty()) {
-            answer[st.peek()] = prices.length - st.peek() - 1;
-            st.pop();
-        }
+//         for(int i = 0; i < prices.length; i++) {
+//             // 가격 떨어졌다면
+//             while(!st.isEmpty() && prices[st.peek()] > prices[i]) {
+//                 answer[st.peek()] = i - st.peek();
+//                 st.pop();
+//             }
+//             st.push(i);    
+//         }
+        
+//         while(!st.isEmpty()) {
+//             answer[st.peek()] = prices.length - st.peek() - 1;
+//             st.pop();
+//         }
         
         return answer;
     }
