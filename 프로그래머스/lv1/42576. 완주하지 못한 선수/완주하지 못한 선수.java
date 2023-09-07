c577a8ce8a5ca275937d7bf04ee7448d97459a7a
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-09-07 11:56:11 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 358.36 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
+      
         Arrays.sort(participant);
         Arrays.sort(completion);
-        LinkedList<String> arr = new LinkedList<>(Arrays.asList(participant));
         
         for(int i = 0; i < completion.length; i++)
-            arr.remove(completion[i]);
-        answer = arr.get(0);
-        return answer;
+            if(!completion[i].equals(participant[i]))
+                return participant[i];
+        
+        return participant[participant.length - 1];
     }
 }
