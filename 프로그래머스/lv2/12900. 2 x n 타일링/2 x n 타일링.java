f61d155f7844f929016ec4a8d01228e21fa313b7
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-09-11 18:22:47 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.27 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -2,14 +2,10 @@ class Solution {
     public int solution(int n) {
         int dp[] = new int[n + 1];
         
-        if(n == 1 || n == 2) return 2;
-        if(n == 3) return 3;
-        
-        dp[1] = 2;
+        dp[1] = 1;
         dp[2] = 2;
-        dp[3] = 3;
         
-        for(int i = 4; i <= n; i++)
+        for(int i = 3; i <= n; i++)
             dp[i] = (dp[i-2] + dp[i-1]) % 1000000007;
         
         return dp[n];
