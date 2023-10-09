d810e06a817398a84e7abd931443a875be6fd7be
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-10-09 21:22:21 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 684 ms, Memory: 56688 KB -BaekjoonHub

---
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine()); 
+	    StringBuilder sb = new StringBuilder();
+	    
+	    int N = Integer.parseInt(st.nextToken());
+	    int M = Integer.parseInt(st.nextToken());
+	    String name[][] = new String[N][2];
+	    for(int i = 0; i < N; i++) {
+	        st = new StringTokenizer(br.readLine());
+	        name[i][0] = st.nextToken();
+    	    name[i][1] = st.nextToken();
+	    }
+	   
+	   for(int i = 0; i < M; i++) {
+	       int power = Integer.parseInt(br.readLine());
+	       
+	       int start = 0;
+	       int end = N - 1;
+	       int mid = 0;
+	       while(start <= end) {
+	           mid = (start + end) / 2;
+	           
+	           int n = Integer.parseInt(name[mid][1]);
+	           
+	           if(power > n)
+	                start = mid + 1;
+	           else
+	                end = mid - 1;
+	       }
+	       sb.append(name[start][0]).append("\n");
+	   }
+	        
+	    System.out.println(sb);    
+	}
+	
+}
