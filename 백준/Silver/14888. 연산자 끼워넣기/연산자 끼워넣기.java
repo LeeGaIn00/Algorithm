e8b69bdf0d7439665daf1a29e143e3a195aa4399
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-11-13 22:17:46 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 128 ms, Memory: 15180 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,31 +1,46 @@
 import java.util.*;
+import java.io.*;
 
 public class Main
 {
-    static int n;
+    static int N;
     static int num[];
     static int op[] = new int[4];
+    
     static int MAX = Integer.MIN_VALUE;
     static int MIN = Integer.MAX_VALUE;
-   
-	public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
-    	n = sc.nextInt();
-    	num = new int[n];
-    	for(int i = 0; i < n; i++)
-    	    num[i] = sc.nextInt();
-    	for(int i = 0; i < 4; i++)
-    	    op[i] = sc.nextInt();
-    	dfs(num[0], 1);
-    	System.out.println(MAX);
-    	System.out.println(MIN);
+    
+    static StringBuilder sb = new StringBuilder();
+    
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    N = Integer.parseInt(br.readLine());
+	    num = new int[N];
+	    
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+	    for(int i = 0; i < N; i++)
+	        num[i] = Integer.parseInt(st.nextToken());
+	        
+	    st = new StringTokenizer(br.readLine());
+	    
+	    op[0] = Integer.parseInt(st.nextToken());
+	    op[1] = Integer.parseInt(st.nextToken());
+	    op[2] = Integer.parseInt(st.nextToken());
+	    op[3] = Integer.parseInt(st.nextToken());
+	    
+	    dfs(num[0], 1);
+	    
+	    System.out.println(MAX);
+	    System.out.println(MIN);
 	}
+	
 	static void dfs(int res, int depth) {
-	    if(depth == n) {
+	    if(depth == N) {
 	        MAX = Math.max(MAX, res);
 	        MIN = Math.min(MIN, res);
 	        return;
 	    }
+	    
 	    for(int i = 0; i < 4; i++) {
 	        if(op[i] > 0) {
 	            op[i]--;
