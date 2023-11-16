704d966daff2e72e31048e5c6341914f66fd4342
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-11-17 00:02:04 +0900

    [Silver V] Title: 폴리오미노, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.java
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -5,16 +5,32 @@ public class Main
 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    String str = br.readLine();
 	    StringBuilder sb = new StringBuilder();
 	    
-	    str = str.replaceAll("XXXX", "AAAA").replaceAll("XX", "BB");
+	    String str = br.readLine();
 	    
-	    if(str.contains("X")) 
-	        sb.append(-1);
+	    String[] arr = str.split("\\.");
+	     
+	    for(int i = 0; i < arr.length; i++) {
+	        String s = arr[i];
 	        
-	    else
-	        sb.append(str);
+	        if(s.length() % 2 != 0) {
+	            System.out.println(-1);
+	            return;
+	        }
+	        
+	        else if(s.length() % 4 == 0)
+	            sb.append("AAAA".repeat(s.length() / 4));
+	            
+	        else
+	            sb.append("AAAA".repeat(s.length() / 4)).append("BB");
+	            
+	        if(i != arr.length - 1)
+	            sb.append(".");
+	        
+	    }
+	    
+	    sb.append(".".repeat(str.length() - sb.toString().length())); 
 	    
 	    System.out.println(sb);
 	}
