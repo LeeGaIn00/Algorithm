420d7ce0f9ce0ac5bc3f6c247c8d21d5728ee47e
LeeGaIn00 <73627509+LeeGaIn00@users.noreply.github.com>
2023-12-25 00:10:47 +0900

    [Bronze IV] Title: 모음의 개수, Time: 120 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.java
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.java
@@ -1,31 +1,27 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        for(;;) {
-            String str = sc.nextLine();
-            int count = 0;
-            if(str.equals("#")) {
+public class Main
+{
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    
+	    while(true) {
+	        String str = br.readLine();
+	        int cnt = 0;
+	        
+	        if(str.equals("#"))
                 break;
-            }
+               
+        
+            for(int i = 0; i < str.length(); i++)
+                if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u'
+                    || str.charAt(i) == 'A' || str.charAt(i) == 'E' || str.charAt(i) == 'I' || str.charAt(i) == 'O' || str.charAt(i) == 'U')
+                    cnt++;
+    
+            
+             System.out.println(cnt);
+	    }
 
-            str = str.toLowerCase();
-
-            for(int i=0; i<str.length(); i++) {
-               switch(str.charAt(i)) {
-                     case 'a':
-                     case 'e':
-                     case 'i':
-                     case 'o':   
-                     case 'u':
-                        count++;
-                    default: 
-                        break;
-               }
-            }            
-            System.out.println(count);
-        }
-    }
+	}
 }
